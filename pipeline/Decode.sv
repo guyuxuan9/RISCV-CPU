933707b4c1732a9370820e4ed03ae5f05990b2bf
guyuxuan9 <guyuxuan9@gmail.com>
2022-12-06 19:52:55 +0000

    add decode stage and DE registers to top level

--- pipeline/Decode.sv
+++ pipeline/Decode.sv
@@ -1,16 +1,61 @@
 module Decode #(
     parameter DATA_WIDTH = 32
 )(
-    input logic [DATA_WIDTH-1:0]            InstrD,
-    input logic [31:0]                      PCD,
-    input logic [31:0]                      PCPlus4
+    input logic                             clk,
+    input logic  [DATA_WIDTH-1:0]           InstrD,
+    input logic  [4:0]                      RdW, // output from write back stage
+    input logic                             RegWriteW, // output from write back stage
+    input logic  [DATA_WIDTH-1:0]           ResultW, // output from write back stage
+    output logic [DATA_WIDTH-1:0]           RD1,
+    output logic [DATA_WIDTH-1:0]           RD2,
+    output logic [DATA_WIDTH-1:0]           a0,
+    output logic [4:0]                      RdD,
+    output logic                            RegWriteD,
+    output logic [1:0]                      ResultSrcD,
+    output logic                            MemWriteD,
+    output logic                            JumpD,
+    output logic                            BranchD,
+    output logic                            ALUControlD,
+    output logic                            ALUSrcD,
+    output logic                            ImmSrcD,
+    output logic                            jalrmuxSelD,
+    output logic [DATA_WIDTH-1:0]           ImmExtD
 );
+
+
 
 control_unit cu(
-    .op(InstrD[6:0]),
-    .funct3(InstrD[14:12]),
-    .funct7(InstrD[30]),
-    // to be completed!!!
+    .op(InstrD[6:0]),       // input
+    .funct3(InstrD[14:12]), // input
+    .funct7(InstrD[30]),    // input
+    .RegWrite(RegWriteD),
+    .ResultSrc(ResultSrcD),
+    .MemWrite(MemWriteD),
+    .Jump(JumpD),
+    .Branch(BranchD),
+    .ALUControl(ALUControlD),
+    .ALUSrc(ALUSrcD),
+    .ImmSrc(ImmSrcD),
+    .jalrmuxSel(jalrmuxSelD)
 );
 
+regfile RegFile(
+    .clk(clk),
+    .A1(InstrD[19:15]),
+    .A2(InstrD[24:20]),
+    .A3(RdW),
+    .WE3(RegWriteW),
+    .WD3(ResultW),
+    .RD1(RD1), //output
+    .RD2(RD2), //output
+    .a0(a0) // output
+);
+
+sign_extend Extend(
+    .instr(InstrD[31:7]),
+    .ImmSrc(ImmSrcD),
+    .ImmExt(ImmExtD) // output
+);
+
+assign RdD = InstrD[11:7];
 endmodule
--- pipeline/pipelined_top.sv
+++ pipeline/pipelined_top.sv
@@ -18,6 +18,39 @@ logic [31:0]                    PCD;
 logic [31:0]                    InstrD;
 logic [31:0]                    PCPlus4D;
 
+logic [4:0]                     RdW; // output from write back stage
+logic                           RegWriteW; // output from write back stage
+logic [DATA_WIDTH-1:0]          ResultW; // output from write back stage
+logic [DATA_WIDTH-1:0]          RD1D;
+logic [DATA_WIDTH-1:0]          RD2D;
+logic [4:0]                     RdD;
+
+logic                           clk;
+logic                           RegWriteD;
+logic [1:0]                     ResultSrcD;
+logic                           MemWriteD;
+logic                           JumpD;
+logic                           BranchD;
+logic [2:0]                     ALUControlD;
+logic                           ALUSrcD;
+logic                           JalrmuxSelD;
+logic [DATA_WIDTH-1:0]          ImmExtD;
+
+logic [DATA_WIDTH-1:0]          RD1E;
+logic [DATA_WIDTH-1:0]          RD2E;
+logic [DATA_WIDTH-1:0]          PCE;
+logic [DATA_WIDTH-1:0]          ImmExtE;
+logic [DATA_WIDTH-1:0]          PCPlus4E;
+logic [4:0]                     RdE
+
+logic                           RegWriteE,
+logic [1:0]                     ResultSrcE,
+logic                           MemWriteE,
+logic                           JumpE,
+logic                           BranchE,
+logic [2:0]                     ALUControlE,
+logic                           ALUSrcE,
+logic                           JalrmuxSelE
 
 Fetch fetch(
     .PCSrc(PCSrcE),
@@ -42,5 +75,61 @@ topFD FD_reg(
     .PCPlus4D(PCPlus4D)
 );
 
+Decode decode(
+    .clk(clk),
+    .InstrD(InstrD),
+    .RdW(RdW),
+    .RegWriteW(RegWriteW),
+    .ResultW(ResultW),
+    .RD1(RD1D),
+    .RD2(RD2D),
+    .a0(a0),
+    .RdD(RdD),
+    .RegWriteD(RegWriteD),
+    .ResultSrcD(ResultSrcD),
+    .MemWriteD(MemWriteD),
+    .JumpD(JumpD),
+    .BranchD(BranchD),
+    .ALUControlD(ALUControlD),
+    .ALUSrcD(ALUSrcD),
+    .JalrmuxSelD(JalrmuxSelD),
+    .ImmExtD(ImmExtD)
+);
+
+topDE DE_reg(
+    .clk(clk),
+    .RD1D(RD1D),
+    .RD2D(RD2D),
+    .PCD(PCD),
+    .ImmExtD(ImmExtD),
+    .PCPlus4D(PCPlus4D),
+    .RdD(RdD),  // A3
+    .RD1E(RD1E),
+    .RD2E(RD2E),
+    .PCE(PCE),
+    .ImmExtE(ImmExtE),
+    .PCPlus4E(PCPlus4E),
+    .RdE(RdE)
+);
+
+top cuDE(
+    .clk(clk),
+    .RegWriteD(RegWriteD),
+    .ResultSrcD(ResultSrcD),
+    .MemWriteD(MemWriteD),
+    .JumpD(JumpD),
+    .BranchD(BranchD),
+    .ALUControlD(ALUControlD),
+    .ALUSrcD(ALUSrcD),
+    .JalrmuxSelD(JalrmuxSelD),
+    .RegWriteE(RegWriteE),
+    .ResultSrcE(ResultSrcE),
+    .MemWriteE(MemWriteE),
+    .JumpE(JumpE),
+    .BranchE(BranchE),
+    .ALUControlE(ALUControlE),
+    .ALUSrcE(ALUSrcE),
+    .JalrmuxSelE(JalrmuxSelE)
+);
 
 endmodule
