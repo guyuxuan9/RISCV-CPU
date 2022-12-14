7f7bda7958db43b4ae13efde7797ae5c5ba9ad51
guyuxuan9 <guyuxuan9@gmail.com>
2022-12-14 23:14:33 +0000

    fix some bugs

--- cachecpu/normalcache/cachemem.sv
+++ cachecpu/normalcache/cachemem.sv
@@ -1,6 +1,5 @@
 module cachemem#(
-    parameter   ADDRESS_WIDTH = 3,
-                DATA_WIDTH = 60
+    parameter   DATA_WIDTH = 60
 )(
     input logic     [31:0]        memaddr,
     input logic     [31:0]        inputdata,
@@ -13,12 +12,12 @@ logic hit = 1'b0;
 logic tagCompare;
 logic vbit = 1'b0;
 
-logic [60-1:0] ram_array [7:0]; // [2**8 - 1:0]
+logic [DATA_WIDTH-1:0] ram_array [7:0]; 
 
 always_comb begin
     if (cache_WE)
     begin
-        tagCompare = (~(memaddr[31:5] ^ ram_array[memaddr[4:2]][58:32]));
+        tagCompare = (memaddr[31:5] == ram_array[memaddr[4:2]][58:32]);
         vbit = ram_array[memaddr[4:2]][59];
         hit = (tagCompare & vbit);
         if(hit)
@@ -31,9 +30,9 @@ end
 always_ff @(posedge clk) begin
     if (hit == 1'b0 & cache_WE == 1'b1)
     begin
-        ram_array[memaddr[4:2]][59] = 1'b1;
-        ram_array[memaddr[4:2]][31:0] = inputdata;
-        ram_array[memaddr[4:2]][58:32] = memaddr[31:5];
+        ram_array[memaddr[4:2]][59] <= 1'b1;
+        ram_array[memaddr[4:2]][31:0] <= inputdata;
+        ram_array[memaddr[4:2]][58:32] <= memaddr[31:5];
     end
 end
 
