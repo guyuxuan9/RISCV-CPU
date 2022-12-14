41ae6332f5ccc200a0351dd7aa8b61475e7d5da3
shindelee <jrsdlee@gmail.com>
2022-12-14 11:29:48 +0000

    pipeline reference testing

--- pipeline/pdf.asm
+++ pipeline/pdf.asm
@@ -1,4 +1,3 @@
-
 pdf.out.reloc:     file format elf32-littleriscv
 
 
@@ -6,84 +5,86 @@ Disassembly of section .text:
 
 bfc00000 <main> (File Offset: 0x1000):
 main():
-bfc00000:	010000ef          	jal	ra,bfc00010 <init> (File Offset: 0x1010)
-                              addi zero, zero, 0
-bfc00004:	020000ef          	jal	ra,bfc00024 <build> (File Offset: 0x1024)
-                              addi zero, zero, 0
+bfc00000:	020000ef          	jal	ra, bfc00020 <init> (File Offset: 0x1010)
+bfc00004: 00000013            addi zero, zero, 0
+bfc00008:	058000ef          	jal	ra, bfc00058 <build> (File Offset: 0x1024)
+bfc0000c: 00000013            addi zero, zero, 0
 
-bfc00008 <forever> (File Offset: 0x1008):
+bfc00010 <forever> (File Offset: 0x1008):
 forever():
-bfc0000c:	050000ef          	jal	ra,bfc00058 <display> (File Offset: 0x1058)
-                              addi zero, zero, 0
-bfc00010:	ffdff06f          	jal	zero,bfc00008 <forever> (File Offset: 0x1008)
-                              addi zero, zero, 0
+bfc00010:	0b8000ef          	jal	ra, bfc000b8 <display> (File Offset: 0x1058)
+bfc00014: 00000013            addi zero, zero, 0
+bfc00018:	0100006f          	jal	zero, bfc00010 <forever> (File Offset: 0x1008)
+bfc0001c: 00000013            addi zero, zero, 0
 
-bfc00014 <init> (File Offset: 0x1010):
+bfc00020 <init> (File Offset: 0x1010):
 init():
-bfc00018:	10000593          	addi	a1,zero,256
-                              addi zero, zero, 0
-                              addi zero, zero, 0
+bfc00020:	10000593          	addi	a1,zero,256
+bfc00024: 00000013            addi zero, zero, 0
+bfc00028: 00000013            addi zero, zero, 0
 
-bfc00014 <_loop1> (File Offset: 0x1014):
+bfc0002c <_loop1> (File Offset: 0x1014):
 _loop1():
-bfc00014:	fff58593          	addi	a1, a1, -1
-                              addi zero, zero, 0
-                              addi zero, zero, 0
-bfc00018:	10058023          	sb	zero, 256(a1)
-                              addi zero, zero, 0
-                              addi zero, zero, 0
-bfc0001c:	fe059ce3          	bne	a1,zero,bfc00014 <_loop1> (File Offset: 0x1014)
-                              addi zero, zero, 0
-                              addi zero, zero, 0
-bfc00020:	00008067          	jalr	zero,0(ra)
-                              addi zero, zero, 0
+bfc0002c:	fff58593          	addi a1, a1, -1
+bfc00030: 00000013            addi zero, zero, 0
+bfc00034: 00000013            addi zero, zero, 0
+bfc00038:	10058023          	sb	zero, 256(a1)
+bfc0003c: 00000013            addi zero, zero, 0
+bfc00040: 00000013            addi zero, zero, 0
+bfc00044:	fe059ce3          	bne	a1,zero, bfc0002c <_loop1> (File Offset: 0x1014)
+bfc00048: 00000013            addi zero, zero, 0
+bfc0004c: 00000013            addi zero, zero, 0
+bfc00050:	00008067          	jalr	zero, 0(ra)
+bfc00054: 00000013            addi zero, zero, 0
 
-bfc00024 <build> (File Offset: 0x1024):
+bfc00058 <build> (File Offset: 0x1024):
 build():
-bfc00024:	000105b7          	lui	a1,0x10
-bfc00028:	00000613          	addi	a2,zero,0
-bfc0002c:	10000693          	addi	a3,zero,256
-bfc00030:	0c800713          	addi	a4,zero,200
+bfc00058:	000105b7          	lui	a1,0x10
+bfc0005c:	00000613          	addi	a2,zero,0
+bfc00060:	10000693          	addi	a3,zero,256
+bfc00064:	0c800713          	addi	a4,zero,200
 
-bfc00034 <_loop2> (File Offset: 0x1034):
+bfc00068 <_loop2> (File Offset: 0x1034):
 _loop2():
-bfc00034:	00c587b3          	add	a5,a1,a2
-                              addi zero, zero, 0
-                              addi zero, zero, 0
-bfc00038:	0007c283          	lbu	t0,0(a5)
-                              addi zero, zero, 0
-                              addi zero, zero, 0
-bfc0003c:	00d28833          	add	a6,t0,a3
-                              addi zero, zero, 0
-                              addi zero, zero, 0
-bfc00040:	00084303          	lbu	t1,0(a6)
-                              addi zero, zero, 0
-                              addi zero, zero, 0
-bfc00044:	00130313          	addi	t1,t1,1
-                              addi zero,zero,0
-                              addi zero,zero,0
-bfc00048:	00680023          	sb	t1,0(a6)
-bfc0004c:	00160613          	addi	a2,a2,1
-bfc00050:	fee312e3          	bne	t1,a4,bfc00034 <_loop2> (File Offset: 0x1034)
-bfc00054:	00008067          	jalr	zero,0(ra)
+bfc00068:	00c587b3          	add	a5, a1, a2
+bfc0006c: 00000013            addi zero, zero, 0
+bfc00070: 00000013            addi zero, zero, 0
+bfc00074:	0007c283          	lbu	t0, 0(a5)
+bfc00078: 00000013            addi zero, zero, 0
+bfc0007c: 00000013            addi zero, zero, 0
+bfc00080:	00d28833          	add	a6, t0, a3
+bfc00084: 00000013            addi zero, zero, 0
+bfc00088: 00000013            addi zero, zero, 0
+bfc0008c:	00084303          	lbu	t1, 0(a6)
+bfc00090: 00000013            addi zero, zero, 0
+bfc00094: 00000013            addi zero, zero, 0
+bfc00098:	00130313          	addi t1, t1, 1
+bfc0009c: 00000013            addi zero,zero,0
+bfc000a0: 00000013            addi zero,zero,0
+bfc000a4:	00680023          	sb t1, 0(a6)
+bfc000a8:	00160613          	addi a2, a2, 1
+bfc000ac:	fee312e3          	bne	t1, a4, bfc00068 <_loop2> (File Offset: 0x1034)
+bfc000b0:	00008067          	jalr	zero, 0(ra)
+bfc000b4: 00000013            addi zero, zero, 0
 
-bfc00058 <display> (File Offset: 0x1058):
+bfc000b8 <display> (File Offset: 0x1058):
 display():
-bfc00058:	00000593          	addi	a1,zero,0
-                              addi zero, zero, 0
-bfc0005c:	0ff00613          	addi	a2,zero,255
+bfc000b8:	00000593          	addi	a1, zero, 0
+bfc000bc: 00000013            addi zero, zero, 0
+bfc000c0:	0ff00613          	addi	a2, zero, 255
 
-bfc00060 <_loop3> (File Offset: 0x1060):
+bfc000c4 <_loop3> (File Offset: 0x1060):
 _loop3():
-bfc00060:	1005c503          	lbu	a0,256(a1) # 10100 <base_data+0x100> (File Offset: 0xffffffff40411100)
-bfc00064:	00158593          	addi	a1,a1,1
-bfc00068:                     addi zero,zero,0
-bfc0006c:                     addi zero,zero,0
-bfc00068:	fec59ce3          	bne	a1,a2,bfc00060 <_loop3> (File Offset: 0x1060)
-                              addi zero, zero, 0
-                              addi zero, zero, 0
-bfc0006c:	00008067          	jalr	zero, 0(ra)
-                              addi zero, zero, 0
+bfc000c4:	1005c503          	lbu	a0,256(a1) # 10100 <base_data+0x100> (File Offset: 0xffffffff40411100)
+bfc000c8:	00158593          	addi	a1, a1, 1
+bfc000cc: 00000013            addi zero, zero, 0
+bfc000d0: 00000013            addi zero, zero, 0
+bfc000d4:	fec59ce3          	bne	a1,a2, bfc000c4 <_loop3> (File Offset: 0x1060)
+bfc000d8: 00000013            addi zero, zero, 0
+bfc000dc: 00000013            addi zero, zero, 0
+bfc000e0:	00008067          	jalr	zero, 0(ra)
+bfc000e4: 00000013            addi zero, zero, 0
+
 
 Disassembly of section .riscv.attributes:
 
